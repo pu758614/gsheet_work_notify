88f060aa915cee83866992c0d05dccb5a1cd767a
shiun <pu758614@gmail.com>
2021-11-19 00:58:04 +0800

    make it better

--- lib/lib.php
+++ lib/lib.php
@@ -27,12 +27,16 @@
 
         function getGoogleSheet($id){
             // 建立 Google Client
-            exit;
+            echo "1<br>";
             $client = new \Google_Client();
+            echo "2<br>";
             $client->setApplicationName('Google Sheets and PHP');
+            echo "3<br>";
             // 設定權限
             $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+            echo "4<br>";
             $client->setAccessType('offline');
+            echo "5<br>";
             // 引入金鑰
             $config = array(
                 "type"         => isset($_ENV['GOOGLE_SHEET_TYPE'])?$_ENV['GOOGLE_SHEET_TYPE']:'',
@@ -41,20 +45,23 @@
                 "private_key"  => isset($_ENV['GOOGLE_SHEET_PRIVATE_KEY'])?$_ENV['GOOGLE_SHEET_PRIVATE_KEY']:'',
             );
             $client->setAuthConfig($config);
-
+            echo "6<br>";
 
             // 建立 Google Sheets Service
             $service = new \Google_Service_Sheets($client);
             // 取得 Sheet 範圍
             $getRange = "A:P";
-
+            echo "7<br>";
             // 讀取資料
             $data = array();
             try {
+                echo "8<br>";
                 $response = $service->spreadsheets_values->get($id, $getRange);
                 $data = $response->getValues();
+                echo "9<br>";
             } catch (Exception $e) {
                 print json_encode($e->getMessage(),JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+                echo "10<br>";
             }
             return $data;
         }
