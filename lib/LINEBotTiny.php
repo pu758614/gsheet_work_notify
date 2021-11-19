1fd245d3a6b1c1febd5d970a21ce0711758256af
shiun <pu758614@gmail.com>
2021-11-20 01:34:21 +0800

    make it better

--- lib/LINEBotTiny.php
+++ lib/LINEBotTiny.php
@@ -90,16 +90,12 @@ class LINEBotTiny
         $response = $bot->pushMessage($user_id, $textMessageBuilder);
 
         //echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-        error_log($user_id.'XDDDDDD:: ' . $response->getHTTPStatus() . ' ' . $response->getRawBody());
+        error_log('XDDDDDD:: ' . $response->getHTTPStatus() . ' ' . $response->getRawBody());
     }
 
 
 
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('<to>', $textMessageBuilder);
     public function reply_text($replyToken,$text=''){
         $this->replyMessage(array(
             'replyToken' => $replyToken,
