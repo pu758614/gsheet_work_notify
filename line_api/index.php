91a4e4210c19b728437dee2e304831932e840fdc
shiun <pu758614@gmail.com>
2021-11-20 15:01:47 +0800

    make it better

--- line_api/index.php
+++ line_api/index.php
@@ -34,88 +34,77 @@ foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
-
-            switch ($message['type']) {
-                case 'text':
-
-                    $str     = $message['text'];
-                    $str     = strtolower($str);
-                    $str     = convertStrType($str);
-                    $str     = str_replace(' ','',$str);
-                    $str_arr = explode(":",$str);
-                    $action  = isset($str_arr[0])?$str_arr[0]:'';
-                    $val     = isset($str_arr[1])?$str_arr[1]:'';
-                    $change_week_day_cn_conf = array(
-                        '0' => "日",
-                        '1' => "一",
-                        '2' => "二",
-                        '3' => "三",
-                        '4' => "四",
-                        '5' => "五",
-                        '6' => "六",
-                    );
-                    if(count($str_arr)==2 && $action=='set'){
-                        $update_data = array(
-                            "modify_time" => date('Y-m-d H:i:s')
-                        );
-                        if(isset($change_week_day_cn_conf[$val])){
-                            $update_data['notify_day'] = $val;
-                            $msg = "您的提醒時間為每週".$change_week_day_cn_conf[$val];
-                        }else if($val=='on'){
-                            $update_data['enable_notify'] = 1;
-                            $msg = "已經啟動提醒，提醒時間為每週".$change_week_day_cn_conf[$user_data['notify_day']];
-                        }else if($val=='off'){
-                            $update_data['enable_notify'] = 0;
-                            $msg = "已經關閉提醒";
-                        }
-                        if(count($update_data)>=2){
-                            $result = $db->updateData("sheet_notify_user",$update_data,array("id"=>$user_id));
-                            $send_msg = '';
-                            if($result){
-                                $send_msg = "設定成功，".$msg;
-                            }else{
-                                $send_msg = "設定失敗";
-                            }
-                            $result = $client->reply_text($event['replyToken'],$send_msg);
-                            $db->insertData("sheet_notify_notify_log",array(
-                                "line_user_uuid" => $user_data['line_user_uuid'],
-                                "type"           => "set",
-                                "real_name"      => $user_data['real_name'],
-                                "msg"            => $send_msg,
-                                "response"       => $result['msg'],
-                                "status"         => $result['status'],
-                                "create_time"    => date('Y-m-d H:i:s')
-                            ));
-                        }
+            if($message['type']!='text'){
+                continue;
+            }
+            $str     = $message['text'];
+            $str     = strtolower($str);
+            $str     = convertStrType($str);
+            $str     = str_replace(' ','',$str);
+            $str_arr = explode(":",$str);
+            $action  = isset($str_arr[0])?$str_arr[0]:'';
+            $val     = isset($str_arr[1])?$str_arr[1]:'';
+            $change_week_day_cn_conf = array(
+                '0' => "日",
+                '1' => "一",
+                '2' => "二",
+                '3' => "三",
+                '4' => "四",
+                '5' => "五",
+                '6' => "六",
+            );
+            if(count($str_arr)==2 && $action=='set'){
+                $update_data = array(
+                    "modify_time" => date('Y-m-d H:i:s')
+                );
+                if(isset($change_week_day_cn_conf[$val])){
+                    $update_data['notify_day'] = $val;
+                    $msg = "您的提醒時間為每週".$change_week_day_cn_conf[$val];
+                }else if($val=='on'){
+                    $update_data['enable_notify'] = 1;
+                    $msg = "已經啟動提醒，提醒時間為每週".$change_week_day_cn_conf[$user_data['notify_day']];
+                }else if($val=='off'){
+                    $update_data['enable_notify'] = 0;
+                    $msg = "已經關閉提醒";
+                }
+                if(count($update_data)>=2){
+                    $result = $db->updateData("sheet_notify_user",$update_data,array("id"=>$user_id));
+                    $send_msg = '';
+                    if($result){
+                        $send_msg = "設定成功，".$msg."。";
+                    }else{
+                        $send_msg = "設定失敗。";
                     }
+                    $result = $client->reply_text($event['replyToken'],$send_msg);
+                    $db->insertData("sheet_notify_notify_log",array(
+                        "line_user_uuid" => $user_data['line_user_uuid'],
+                        "type"           => "set",
+                        "real_name"      => $user_data['real_name'],
+                        "msg"            => $send_msg,
+                        "response"       => $result['msg'],
+                        "status"         => $result['status'],
+                        "create_time"    => date('Y-m-d H:i:s')
+                    ));
+                }
+            }
 
-                    if($message['text']=='三民聖教會'){
-
-                        $client->replyMessage([
-                            'replyToken' => $event['replyToken'],
-                            'messages' => [
-                                [
-                                    "type"=>"location",
-                                    "title"=>"灣告輝底家啦！！",
-                                    "address"=>"813左營區重立路61號",
-                                    "latitude"=>'22.673217',
-                                    "longitude"=>'120.313176'
-                                ]
-                            ]
-                        ]);
-                        break;
-                    }
-
-
-
-                    break;
-                default:
-                    error_log('Unsupported message type: ' . $message['type']);
-                    break;
+            if($message['text']=='三民聖教會'){
+                $client->replyMessage([
+                    'replyToken' => $event['replyToken'],
+                    'messages' => [
+                        [
+                            "type"=>"location",
+                            "title"=>"灣告輝底家啦！！",
+                            "address"=>"813左營區重立路61號",
+                            "latitude"=>'22.673217',
+                            "longitude"=>'120.313176'
+                        ]
+                    ]
+                ]);
+                break;
             }
             break;
         default:
-
             error_log('Unsupported event type: ' . $event['type']);
             break;
     }
