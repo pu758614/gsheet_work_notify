d1563f9fa5ba4f58682405cfab790b8ad12a0726
shiun <pu758614@gmail.com>
2021-12-12 02:03:18 +0800

    make it better

--- lib/db_crud.php
+++ lib/db_crud.php
@@ -43,7 +43,7 @@ Trait DB_CRUD {
         $arr_prestr = array($value);
         $sql = "SELECT * FROM $table WHERE $key=? $order_by";
         $result = $this->db->Execute($sql,$arr_prestr);
-        if($result && $result->RecordCount() > 0){
+        if($result){
             return $result->getAll();
         }else{
             return array();
