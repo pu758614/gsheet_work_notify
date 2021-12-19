4a8e32c230e2c12cc282176e2c760cab7017ca6c
shiun <pu758614@gmail.com>
2021-12-19 23:08:14 +0800

    make it better

--- system_set.php
+++ system_set.php
@@ -1,16 +1,16 @@
 <?php
 
-    $conf_list = array(
-        "pass_word",
-        "sheet_url",
-        "sheet_code",
-    );
-    $assign_list = array();
-    foreach ($conf_list as  $conf_key) {
-        $data = $db->getSingleById('sheet_notify_config',"item",$conf_key);
-        if(empty($data)){
-            continue;
-        }
-        $assign_list[$conf_key] = $data['value'];
-    }
-    $tpl->assignArray($assign_list);
+    // $conf_list = array(
+    //     "pass_word",
+    //     "sheet_url",
+    //     "sheet_code",
+    // );
+    // $assign_list = array();
+    // foreach ($conf_list as  $conf_key) {
+    //     $data = $db->getSingleById('sheet_notify_config',"item",$conf_key);
+    //     if(empty($data)){
+    //         continue;
+    //     }
+    //     $assign_list[$conf_key] = $data['value'];
+    // }
+    // $tpl->assignArray($assign_list);
