cb15f7e58e8facb5b4133489e1cb4e79e235fca1
shiun <pu758614@gmail.com>
2022-02-11 00:09:05 +0800

    make it better

--- line_api/index.php
+++ line_api/index.php
@@ -48,71 +48,64 @@ foreach ($client->parseEvents() as $event) {
             $text = strtolower($text);
             $text = convertStrType($text);
             $text = str_replace(' ','',$text);
-            switch ($text) {
-                case 'all':
+            $str_arr = explode(":",$text);
+            $action  = isset($str_arr[0])?$str_arr[0]:'';
+            $val     = isset($str_arr[1])?$str_arr[1]:'';
+            $change_week_day_cn_conf = array(
+                '0' => "日",
+                '1' => "一",
+                '2' => "二",
+                '3' => "三",
+                '4' => "四",
+                '5' => "五",
+                '6' => "六",
+            );
+            $update_data = array(
+                "modify_time" => date('Y-m-d H:i:s')
+            );
+            if(count($str_arr)==2 && $action=='set' && isset($change_week_day_cn_conf[$val])){
+                $update_data['notify_day'] = $val;
+                $msg = "您的提醒時間為每週".$change_week_day_cn_conf[$val];
+            }
 
+            if($text=='on'){
+                $update_data['enable_notify'] = 1;
+                $msg = "已經啟動提醒，提醒時間為每週".$change_week_day_cn_conf[$user_data['notify_day']];
+            }else if($text=='off'){
+                $update_data['enable_notify'] = 0;
+                $msg = "已經關閉提醒";
+            }
 
-                default:
-                    $str_arr = explode(":",$text);
-                    $action  = isset($str_arr[0])?$str_arr[0]:'';
-                    $val     = isset($str_arr[1])?$str_arr[1]:'';
-                    $change_week_day_cn_conf = array(
-                        '0' => "日",
-                        '1' => "一",
-                        '2' => "二",
-                        '3' => "三",
-                        '4' => "四",
-                        '5' => "五",
-                        '6' => "六",
-                    );
-                    $update_data = array(
-                        "modify_time" => date('Y-m-d H:i:s')
-                    );
-                    if(count($str_arr)==2 && $action=='set' && isset($change_week_day_cn_conf[$val])){
-                        $update_data['notify_day'] = $val;
-                        $msg = "您的提醒時間為每週".$change_week_day_cn_conf[$val];
-                    }
-
-                    if($text=='on'){
-                        $update_data['enable_notify'] = 1;
-                        $msg = "已經啟動提醒，提醒時間為每週".$change_week_day_cn_conf[$user_data['notify_day']];
-                    }else if($text=='off'){
-                        $update_data['enable_notify'] = 0;
-                        $msg = "已經關閉提醒";
-                    }
-
-                    if(count($update_data)>=2){
-                        $result = $db->updateData("sheet_notify_user",$update_data,array("id"=>$user_id));
-                        $send_msg = '';
-                        if($result){
-                            $send_msg = "設定成功，".$msg."。";
-                        }else{
-                            $send_msg = "設定失敗。";
-                        }
-                        $result = $client->reply_text($event['replyToken'],$send_msg);
-                        $db->insertData("sheet_notify_notify_log",array(
-                            "line_user_uuid" => $user_data['line_user_uuid'],
-                            "type"           => "set",
-                            "real_name"      => $user_data['real_name'],
-                            "msg"            => $send_msg,
-                            "response"       => $result['msg'],
-                            "status"         => $result['status'],
-                            "create_time"    => date('Y-m-d H:i:s')
-                        ));
-                    }else{
-                        $send_msg = "錯誤的指令，可以輸入「?」來查看指令說明。";
-                        $result = $client->reply_text($event['replyToken'],$send_msg);
-                        $db->insertData("sheet_notify_notify_log",array(
-                            "line_user_uuid" => $user_data['line_user_uuid'],
-                            "type"           => "error",
-                            "real_name"      => $user_data['real_name'],
-                            "msg"            => $send_msg,
-                            "response"       => $result['msg'],
-                            "status"         => $result['status'],
-                            "create_time"    => date('Y-m-d H:i:s')
-                        ));
-                    }
-                    break;
+            if(count($update_data)>=2){
+                $result = $db->updateData("sheet_notify_user",$update_data,array("id"=>$user_id));
+                $send_msg = '';
+                if($result){
+                    $send_msg = "設定成功，".$msg."。";
+                }else{
+                    $send_msg = "設定失敗。";
+                }
+                $result = $client->reply_text($event['replyToken'],$send_msg);
+                $db->insertData("sheet_notify_notify_log",array(
+                    "line_user_uuid" => $user_data['line_user_uuid'],
+                    "type"           => "set",
+                    "real_name"      => $user_data['real_name'],
+                    "msg"            => $send_msg,
+                    "response"       => $result['msg'],
+                    "status"         => $result['status'],
+                    "create_time"    => date('Y-m-d H:i:s')
+                ));
+            }else{
+                $send_msg = "錯誤的指令，可以輸入「?」來查看指令說明。";
+                $result = $client->reply_text($event['replyToken'],$send_msg);
+                $db->insertData("sheet_notify_notify_log",array(
+                    "line_user_uuid" => $user_data['line_user_uuid'],
+                    "type"           => "error",
+                    "real_name"      => $user_data['real_name'],
+                    "msg"            => $send_msg,
+                    "response"       => $result['msg'],
+                    "status"         => $result['status'],
+                    "create_time"    => date('Y-m-d H:i:s')
+                ));
             }
 
             break;
@@ -166,7 +159,6 @@ foreach ($client->parseEvents() as $event) {
                     $msg = $user_data['real_name']."平安！ 以下是您這季接下來的服事，請預備心呦~\n";
                     $work_sheet = array();
                     foreach ($work_list as $work_date => $work_val) {
-                       // $msg .= $work_date."  ". implode('、',$work_val)."\n";
                         $work_sheet[] = $work_date."  ". implode('、',$work_val);
                     }
                     $msg .= implode("\n",$work_sheet);
@@ -177,11 +169,7 @@ foreach ($client->parseEvents() as $event) {
                     $msg = "改週一提醒請輸入「set：1」\n改週二提醒請輸入「set：2」\n...依此類推，數字為0~6(週日~週六)\n\n關閉提醒請輸入「off」\n開啟提醒請輸入「on」\n\n*不分大小寫、全半形";
                     $result = $client->reply_text($event['replyToken'],$msg);
                     break;
-                default:
-                    # code...
-                    break;
             }
-           // $result = $client->reply_text($event['replyToken'],json_encode($event,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
             break;
         default:
             error_log('Unsupported event type: ' . $event['type']);
