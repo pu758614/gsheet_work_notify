1affebbfb2768215af2bdb65c5a3f81e9ab6fa19
shiun <pu758614@gmail.com>
2023-01-13 21:45:28 +0800

    make it better

--- main.php
+++ main.php
@@ -95,10 +95,6 @@
 
     }
 
-    echo '<pre>';
-    print_r($user_item_list);
-    echo '</pre>';
-    exit();
     foreach ($user_item_list as $user_id => $items) {
         $user_data = $db->getSingleByArray('sheet_notify_user',array(
             'id'     => $user_id,
